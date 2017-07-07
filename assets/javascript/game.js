044ab2a40d69ae035a792f55048d5a09d5de074e
Zachary Saltzman <zsaltzman@Zacharys-MacBook-Pro-2.local>
2017-07-07 16:44:13 -0400

    Fri 4:44

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Institute Variables
 
-var time = 30;
+var time = 20;
 var intervalId = "";
 var correct = 0;
 var incorrect = 0;
@@ -12,34 +12,46 @@ var question01 = {
 	question: "What is the other name of Experiment 626?",
 	answers: ["Lilo", "Stitch", "Pleakley", "Gantu"],
 	values: ["incorrect", "correct", "incorrect", "incorrect"],
-	correct: "Stitch"
+	correct: "Stitch",
+	image: "./assets/images/stitch.png"
 };
 var question02 = {
 	question: "What are the names of Ursula's eels?",
 	answers: ["Flotsam and Jetsam", "Romeo and Juliet", "Bonnie and Clyde", "Thelma and Louise"],
 	values: ["correct", "incorrect", "incorrect", "incorrect"],
-	correct: "Flotsam and Jetsam"
+	correct: "Flotsam and Jetsam",
+	image: "./assets/images/flotsamandjetsam.jpg"
 };
 var question03 = {
 	question: "Who is the fairest one of them all?",
 	answers: ["Belle", "Ariel", "Cinderella", "Snow White"],
 	values: ["incorrect", "incorrect", "incorrect", "correct"],
-	correct: "Snow White"
+	correct: "Snow White",
+	image: "./assets/images/snowwhite.png"
 };
 var question04 = {
 	question: "Who went from zero to hero?",
 	answers: ["Aladdin", "John Smith", "Hercules", "Simba"],
 	values: ["incorrect", "incorrect", "correct", "incorrect"],
-	correct: "Hercules"
+	correct: "Hercules",
+	image: "./assets/images/hercules.png"
 };
 var question05 = {
 	question: "Who is not a member of Woody's Roundup",
 	answers: ["Buzz Lightyear", "Jessie", "Bullseye", "Stinky Pete"],
 	values: ["correct", "incorrect", "incorrect", "incorrect"],
-	correct: "Buzz Lightyear"
+	correct: "Buzz Lightyear",
+	image: "./assets/images/buzzlightyear.png"
+};
+var question06 = {
+	question: "What is the name of the cup in Beauty and the Beast?",
+	answers: ["Flake", "Splinter", "Notch", "Chip"],
+	values: ["incorrect", "incorrect", "incorrect", "correct"],
+	correct: "Chip",
+	image: "./assets/images/chip.png"
 };
 
-var questionsArray = [question01, question02, question03, question04, question05];
+var questionsArray = [question01, question02, question03, question04, question05, question06];
 
 // Functions
 
@@ -47,7 +59,7 @@ var questionsArray = [question01, question02, question03, question04, question05
 		$(".content-div").empty();
 		var startButton = $("<button>");
 		startButton.text("Start");
-		startButton.addClass("start");
+		startButton.addClass("start btn btn-default answerBtn");
 		$(".content-div").append(startButton);
 	};
 
@@ -62,17 +74,17 @@ var questionsArray = [question01, question02, question03, question04, question05
         stop();
         if (arrayFinder < questionsArray.length-1) {
 	        unanswered++;
-	        $(".question-div").html("Incorrect!");
 	        solutionWrite(questionsArray[arrayFinder]);
+	        $(".question-div").html("Incorrect!");
 	        arrayFinder++;
-	        setTimeout(function () {questionWrite(questionsArray[arrayFinder])}, 3000);
+	        setTimeout(function () {questionWrite(questionsArray[arrayFinder])}, 2000);
       	}
       	else if (arrayFinder < questionsArray.length) {
       		unanswered++;
-	        $(".question-div").html("Incorrect!");
 	        solutionWrite(questionsArray[arrayFinder]);
+	        $(".question-div").html("Incorrect!");
 	        arrayFinder++;
-	        setTimeout(function () {endWrite(questionsArray[arrayFinder])}, 3000);
+	        setTimeout(function () {endWrite(questionsArray[arrayFinder])}, 2000);
       	}
       };
     };
@@ -82,16 +94,16 @@ var questionsArray = [question01, question02, question03, question04, question05
     };
 
 	function questionWrite (obj) {
-		time = 30;
+		time = 20;
 		$(".timer-div").empty();
 		$(".timer-div").html("Time Remaining: " + time + " Seconds");
 		$(".question-div").empty();
 		$(".content-div").empty();
 		run ();
 		$(".question-div").html(obj.question);
-		for (var i = 0; i < 4; i++) {
+		for (var i = 0; i < obj.answers.length; i++) {
 			var answerButton = $("<button>");
-			answerButton.addClass("answer");
+			answerButton.addClass("answer btn btn-default answerBtn");
 			answerButton.text(obj.answers[i]);
 			answerButton.attr("value", obj.values[i]);
 			$(".content-div").append(answerButton);
@@ -102,7 +114,12 @@ var questionsArray = [question01, question02, question03, question04, question05
 	function solutionWrite (obj) {
 		$(".question-div").empty();
 		$(".content-div").empty();
-		$(".content-div").html("The correct answer was " + obj.correct);
+		$(".content-div").html("The correct answer was " + obj.correct + "<br>");
+		var characterImage = $("<img>");
+		characterImage.attr("height", "250");
+		characterImage.attr("src", obj.image);
+		characterImage.addClass("character")
+		$(".content-div").append(characterImage);
 	};
 
 	function startWrite () {
@@ -117,14 +134,14 @@ var questionsArray = [question01, question02, question03, question04, question05
 				$(".question-div").html("Correct!");
 				correct++;
 				arrayFinder++;
-				setTimeout(function () {questionWrite(questionsArray[arrayFinder])}, 3000);
+				setTimeout(function () {questionWrite(questionsArray[arrayFinder])}, 2000);
 			}
 			else if (arrayFinder < questionsArray.length) {
 	      		correct++;
-		        $(".question-div").html("Correct!");
 		        solutionWrite(questionsArray[arrayFinder]);
+		        $(".question-div").html("Correct!");
 		        arrayFinder++;
-		        setTimeout(function () {endWrite(questionsArray[arrayFinder])}, 3000);
+		        setTimeout(function () {endWrite(questionsArray[arrayFinder])}, 2000);
       		}
 		}
 		else if ($(this).attr("value") == "incorrect") {
@@ -134,14 +151,14 @@ var questionsArray = [question01, question02, question03, question04, question05
 				$(".question-div").html("Incorrect!");
 				incorrect++;
 				arrayFinder++;
-				setTimeout(function () {questionWrite(questionsArray[arrayFinder])}, 3000);
+				setTimeout(function () {questionWrite(questionsArray[arrayFinder])}, 2000);
 			}
 			else if (arrayFinder < questionsArray.length) {
 	      		incorrect++;
-		        $(".question-div").html("Incorrect!");
 		        solutionWrite(questionsArray[arrayFinder]);
+		        $(".question-div").html("Incorrect!");
 		        arrayFinder++;
-		        setTimeout(function () {endWrite(questionsArray[arrayFinder])}, 3000);
+		        setTimeout(function () {endWrite(questionsArray[arrayFinder])}, 2000);
       		}
 		}
 	};
@@ -152,7 +169,7 @@ var questionsArray = [question01, question02, question03, question04, question05
 		$(".question-div").html("Here's how you did!");
 		$(".content-div").html("<p> Correct: " + correct + "</p>" + "<p> Incorrect: " + incorrect + "</p>" + "<p> Unanswered: " + unanswered + "</p>");
 		var resetButton = $("<button>");
-		resetButton.addClass("reset");
+		resetButton.addClass("reset btn btn-default answerBtn");
 		resetButton.text("Start Over?");
 		$(".content-div").append(resetButton);
 	}
